aa1cd920080b06e794a1d8922f983e8d424ee327
Laura  Rozo <admin@promoflow.net>
2025-07-10 11:28:00 -0500

    “…”

---
+++ login/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Promoflow</title>
+  </head>
+  <body class="body_login">
+    <header class="header_nav">
+      <div class="data_login">
+
+      </div>
+      <div class="container_logo_login">
+
+      </div>
+    </header>
+    <main>
+
+    </main>
+  </body>
+</html>
