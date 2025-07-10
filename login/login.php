fd141a129739957b3de4bdbd7b8297bf5ae84524
Laura  Rozo <admin@promoflow.net>
2025-07-10 11:31:54 -0500

    “…”

--- login/login.php
+++ login/login.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Promoflow</title>
-  </head>
-  <body class="body_login">
+
+<?php
+$cssTime = filemtime('../../login/login.css'); // ejemplo: '../Home/5.Video/video.css'
+$jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
+?>
+<link rel="stylesheet" href="../../login/login.css?v=<?= $cssTime ?>">
+<body class="body_login">
     <header class="header_nav">
       <div class="data_login">
 
