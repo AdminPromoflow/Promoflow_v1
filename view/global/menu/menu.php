24e5df88f76c6f1b2a67432f33824c4fdfabcbe7
Laura  Rozo <admin@promoflow.net>
2025-07-16 15:11:44 -0500

    Make menu

--- view/global/menu/menu.php
+++ view/global/menu/menu.php
@@ -6,23 +6,31 @@ $jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
 <link rel="stylesheet" href="../../view/global/menu/menu.css?v=<?= $cssTime ?>">
 <!-- ===== HEADER PRINCIPAL DE LA PÁGINA ===== -->
 <header class="header_menu">
-
   <!-- ===== CONTENEDOR DEL HEADER (usa Flexbox para distribución) ===== -->
-
     <!-- ===== TÍTULO DEL SITIO (lado izquierdo) ===== -->
     <div class="title_menu">
       <h1>Promoflow</h1> <!-- Nombre de la aplicación o empresa -->
-    </div>
+      <div class="logo_img">
+        <img src="../../view/global/menu/img/logo.png" alt="">
+      </div>
 
+    </div>
     <!-- ===== LOGO DEL SITIO (lado derecho) ===== -->
-    <div class="logo_menu">
-      <!-- Ruta relativa al logo. La imagen debe estar en: /view/login/header/img/logo.png -->
-      <input class="header_checkbox" type="checkbox" id="open_menu">
-      <label for="open_menu" class="header_open_nav_buttom" role="button">☰</label>
-      <label for="open_menu" class="header_close_nav_buttom" role="button">X</label>
-    </div>
-
 
 </header>
-
+<div class="movil_menu">
+  <h2>Memu</h2>
+  <div class="menu_img">
+    <img src="../../view/global/menu/img/menu.png" alt="">
+  </div>
+  <!-- Ruta relativa al logo. La imagen debe estar en: /view/login/header/img/logo.png -->
+  <ul>
+    <li> <a href="#"></a>User manager</li>
+    <li> <a href="#"></a>W3P</li>
+    <li> <a href="#"></a>Amazon</li>
+    <li> <a href="#"></a>Lanyard</li>
+    <li> <a href="#"></a>Promoflow</li>
+    <li> <a href="#"></a>Logout</li>
+  </ul>
+</div>
 </html>
