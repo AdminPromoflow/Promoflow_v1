2b37a35bc1d08fdb8df1a1cefb4328794edb4249
Aleina Rossui <admin@promoflow.net>
2025-08-16 20:22:10 -0700

    “…”

--- view/login/header/header.php
+++ view/login/header/header.php
@@ -1,9 +1,9 @@
 
 <?php
-$cssTime = filemtime('../../../view/login_W3P/header/header.css'); // ejemplo: '../Home/5.Video/video.css'
+$cssTime = filemtime('header.css'); // ejemplo: '../Home/5.Video/video.css'
 $jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
 ?>
-<link rel="stylesheet" href="../../../view_W3P/login/header/header.css?v=<?= $cssTime ?>">
+<link rel="stylesheet" href="view_W3P/login/header/header.css?v=<?= $cssTime ?>">
 <!-- ===== HEADER PRINCIPAL DE LA PÁGINA ===== -->
 <header class="header_nav">
 
@@ -11,13 +11,13 @@ $jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
 
     <!-- ===== TÍTULO DEL SITIO (lado izquierdo) ===== -->
     <div class="title_nav">
-      <h1>Promoflow test</h1> <!-- Nombre de la aplicación o empresa -->
+      <h1>Promoflow </h1> <!-- Nombre de la aplicación o empresa -->
     </div>
 
     <!-- ===== LOGO DEL SITIO (lado derecho) ===== -->
     <div class="logo_nav">
       <!-- Ruta relativa al logo. La imagen debe estar en: /view/login/header/img/logo.png -->
-      <img src="../../../view/login_W3P/header/img/logo.png" alt="">
+      <img src="img/logo.png" alt="">
     </div>
 
 
