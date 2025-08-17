57d0ec0ff39d4281bd2769ea310ec8247b7a1fdf
Aleina Rossui <admin@promoflow.net>
2025-08-16 20:23:02 -0700

    “…”

--- view/login/header/header.php
+++ view/login/header/header.php
@@ -1,9 +1,9 @@
 
 <?php
-$cssTime = filemtime('header.css'); // ejemplo: '../Home/5.Video/video.css'
+$cssTime = filemtime('../view/login_W3P/header/header.css'); // ejemplo: '../Home/5.Video/video.css'
 $jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
 ?>
-<link rel="stylesheet" href="view_W3P/login/header/header.css?v=<?= $cssTime ?>">
+<link rel="stylesheet" href="../view_W3P/login/header/header.css?v=<?= $cssTime ?>">
 <!-- ===== HEADER PRINCIPAL DE LA PÁGINA ===== -->
 <header class="header_nav">
 
@@ -17,7 +17,7 @@ $jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
     <!-- ===== LOGO DEL SITIO (lado derecho) ===== -->
     <div class="logo_nav">
       <!-- Ruta relativa al logo. La imagen debe estar en: /view/login/header/img/logo.png -->
-      <img src="img/logo.png" alt="">
+      <img src="../view/login_W3P/header/img/logo.png" alt="">
     </div>
 
 
