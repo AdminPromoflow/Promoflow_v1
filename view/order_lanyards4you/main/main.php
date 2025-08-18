fbe856c27af774f8986243452b434e041273ae09
Aleina Rossui <admin@promoflow.net>
2025-08-17 18:09:23 -0700

    “…”

--- view/order_lanyards4you/main/main.php
+++ view/order_lanyards4you/main/main.php
@@ -6,17 +6,18 @@
 $cssTime = filemtime('../../view/order_lanyards4you/main/main.css');
 $jsTime  = filemtime('../../view/order_lanyards4you/main/main.js');
 ?>
+
 <link rel="stylesheet" href="../../view/order_lanyards4you/main/main.css?v=<?= $cssTime ?>">
 
 <section id="main_lanyards4you" class="container_order">
   <h2>Production Orders</h2>
-
 
   <div class="inner-accordion">
     <div class="inner-accordion_header" role="button" aria-expanded="false" tabindex="0">
       Orders
       <span class="inner-arrow">&#9660;</span>
     </div>
+
     <div class="inner-accordion_content" hidden>
       <div class="inner-accordion">
         <div class="inner-accordion_header" role="button" aria-expanded="false" tabindex="0">
@@ -49,7 +50,6 @@ $jsTime  = filemtime('../../view/order_lanyards4you/main/main.js');
       </div>
     </div>
   </div>
-
 </section>
 
 <script src="../../view/order_lanyards4you/main/main.js?v=<?= $jsTime ?>" type="text/javascript"></script>
