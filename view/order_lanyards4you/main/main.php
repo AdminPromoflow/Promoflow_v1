6e5b48e71aae98a6769dc808853de29e9014593a
Aleina Rossui <admin@promoflow.net>
2025-08-24 10:55:27 -0700

    “…”

--- view/order_lanyards4you/main/main.php
+++ view/order_lanyards4you/main/main.php
@@ -12,49 +12,55 @@ $jsTime  = filemtime('../../view/order_lanyards4you/main/main.js');
 <section id="main_lanyards4you" class="container_order">
   <h2>Production Orders</h2>
 
+  <!-- Nivel 1 -->
   <div class="inner-accordion">
     <div class="inner-accordion_header" role="button" aria-expanded="false" tabindex="0">
       Orders
-      <span class="inner-arrow">&#9660;</span>
+      <span class="inner-arrow arrow-level-1">&#9660;</span>
     </div>
 
     <div class="inner-accordion_content" hidden>
+      <!-- Nivel 2 -->
       <div class="inner-accordion">
         <div class="inner-accordion_header" role="button" aria-expanded="false" tabindex="0">
           Orders
-          <span class="inner-arrow">&#9660;</span>
+          <span class="inner-arrow arrow-level-2">&#9660;</span>
         </div>
         <div class="inner-accordion_content" hidden>
           <!-- … todos tus inputs del Job … -->
         </div>
       </div>
 
+      <!-- Otro acordeón en nivel 2 -->
       <div class="inner-accordion">
         <div class="inner-accordion_header" role="button" aria-expanded="false" tabindex="0">
           Orders
-          <span class="inner-arrow">&#9660;</span>
+          <span class="inner-arrow arrow-level-2">&#9660;</span>
         </div>
         <div class="inner-accordion_content" hidden>
           <!-- … todos tus inputs del Job … -->
         </div>
       </div>
 
+      <!-- Acordeón con subniveles -->
       <div class="inner-accordion">
         <div class="inner-accordion_header" role="button" aria-expanded="false" tabindex="0">
           Orders
-          <span class="inner-arrow">&#9660;</span>
+          <span class="inner-arrow arrow-level-2">&#9660;</span>
         </div>
         <div class="inner-accordion_content" hidden>
+          <!-- Nivel 3 -->
           <div class="inner-accordion">
             <div class="inner-accordion_header" role="button" aria-expanded="false" tabindex="0">
               Orders
-              <span class="inner-arrow">&#9660;</span>
+              <span class="inner-arrow arrow-level-3">&#9660;</span>
             </div>
             <div class="inner-accordion_content" hidden>
+              <!-- Nivel 4 -->
               <div class="inner-accordion">
                 <div class="inner-accordion_header" role="button" aria-expanded="false" tabindex="0">
                   Orders
-                  <span class="inner-arrow">&#9660;</span>
+                  <span class="inner-arrow arrow-level-4">&#9660;</span>
                 </div>
                 <div class="inner-accordion_content" hidden>
                   <!-- … todos tus inputs del Job … -->
@@ -67,5 +73,6 @@ $jsTime  = filemtime('../../view/order_lanyards4you/main/main.js');
     </div>
   </div>
 </section>
+
 
 <script src="../../view/order_lanyards4you/main/main.js?v=<?= $jsTime ?>" type="text/javascript"></script>
