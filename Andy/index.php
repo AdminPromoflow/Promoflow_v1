7f732781a1799a33bf0c87834c41837f4e46b251
Aleina Rossui <admin@promoflow.net>
2026-07-25 22:39:21 -0400

    “…”

--- Andy/index.php
+++ Andy/index.php
@@ -29,7 +29,7 @@
 
 <body>
 
-  <div id="caja_grande" class="container">
+  <div id="container" class="container">
     <div class="top">
       <h1>Title</h1>
     </div>
@@ -39,7 +39,7 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-    <div id="caja_pequeña" class="bottom">
+    <div id="bottom" class="bottom">
       <input id="input" type="text" name="" value="">
     </div>
 
