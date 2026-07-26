f9998db4140672718bd7958ab27a7d2139099189
Aleina Rossui <admin@promoflow.net>
2026-07-25 20:09:38 -0400

    “…”

--- Andy/index.php
+++ Andy/index.php
@@ -5,9 +5,7 @@
 
     <meta charset="UTF-8">
 
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <meta
         name="apple-mobile-web-app-capable"
