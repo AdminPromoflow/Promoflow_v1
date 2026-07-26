af965bd15e48f7de172d186fb555aac47f98bd01
Aleina Rossui <admin@promoflow.net>
2026-07-25 20:32:16 -0400

    “…”

--- Andy/app.js
+++ Andy/app.js
@@ -1,10 +1,13 @@
 const input = document.getElementById("input");
 const cajaGrande = document.getElementById("caja_grande");
 const body = document.body;
+const html = document.documentElement;
 
 input.addEventListener("click", function () {
     alert("hi");
 
     cajaGrande.style.height = "200px";
     body.style.height = "300px";
+    html.style.height = "400px";
+
 });
